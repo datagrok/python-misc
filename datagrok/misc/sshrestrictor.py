2cddaef5c7c73fb7c6b3d4b4d92b7174f5a5be3d
Michael F. Lamb <mike@datagrok.org>
2015-07-15 12:23:21 -0400

    improve sshrestrictor doc a bit

--- datagrok/misc/sshrestrictor.py
+++ datagrok/misc/sshrestrictor.py
@@ -2,8 +2,21 @@
 
 """Limit ssh users to a restricted set of commands.
 
-Installation: Make a script that executes run() this the target of the
-command="..." argument in .ssh/authorized_keys, for the key to be restricted.
+OpenSSH's authorized_keys file supports a "command=" syntax that
+overrides the command that a user is allowed to execute when connecting
+using that key. This alone can be too restrictive for some uses, for
+example if you want to enable some user to run a command with any
+argument, or one of a limited set of commands.
+
+This module is a helper that you may use to build dispatch scripts which
+allow the user's command passed to ssh to be run if it matches one of a
+set of valid patterns. When a match is found, the command is executed
+using the absolute path to the executable you provide.
+
+Installation: make a script that executes run() in this module, and
+assign it as the target of the command="..." argument in
+.ssh/authorized_keys, for the key to be restricted. See the docstring
+for run() for its configuration format.
 
 Example:
 
@@ -11,7 +24,7 @@ Example:
 
         #!/usr/bin/python
         from datagrok.misc.sshrestrictor import run
-        run(valid = [(re.compile(x), c) for (x, c) in [
+        run([
 
             # User may call sendmail
             (r'(?:/usr/sbin/)?sendmail [a-zA-Z0-9. -]*$', '/usr/sbin/sendmail'),
@@ -19,7 +32,7 @@ Example:
             # Or just sleep a bit, useful for establishing tunnels.
             (r'(?:/bin/)?sleep \d?\d?\d$', '/bin/sleep'),
 
-        ]])
+        ])
 
     in .ssh/authorized_keys:
 
@@ -33,25 +46,24 @@ import re
 __author__ = 'Michael F. Lamb <mike@datagrok.org>'
 __date__ = 'Wed, 27 Feb 2008 03:12:54 -0400'
 
-# TODO: support optional validation and/or re-writing of arguments instead of
-# passing them directly to executable.
+# TODO: support optional validation and/or re-writing of arguments
+# instead of passing them directly to executable.
 
-def main(valid):
-    """Match requested command against list of patterns, first to match is
-    executed. Otherwise exit with nonzero status.
+# TODO: unit tests, more examples.
+
+def run(valid):
+    """Match requested command against list of patterns, first to match
+    is executed. Otherwise exit with nonzero status.
 
     valid is a list of (re, path) 2-tuples where:
 
-        re is a compiled regular expression object (or any other object with a
-        .match(string) method accepting one argument).
+        re is a compiled regular expression object (or any other object
+        with a .match(string) method accepting one argument), or a
+        regular expression string to be compiled into an expression.
 
         path is the full path to the acutal executable to be run.
 
     """
-
-    # TODO: make 'valid' more robust, so that it might accept (string, string)
-    # pairs in addition to (re_object, string) pairs. Similar to django's URL
-    # matching mechanism.
 
     try:
         cmd = os.environ['SSH_ORIGINAL_COMMAND']
@@ -59,8 +71,11 @@ def main(valid):
         raise SystemExit("Permission denied for interactive shell.")
 
     for rx, c in valid_re:
+        if isinstance(rx, basestring):
+            rx = re.compile(rx)
         if rx.match(cmd):
             # TODO: support a mechanism for rewriting arguments
+            # TODO: split() is not good enough; use shlex
             os.execvp(c, cmd.split())
             # os.execvp replaces this process with new; script essentially ends
             # here. Imagine a call to sys.exit().
