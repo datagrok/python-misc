adfd10d990272f7620df97566c08a54530794b10
Michael F. Lamb <mike@datagrok.org>
2016-10-03 00:30:06 -0700

    add if __name__=='__main__' in lev.py

--- datagrok/misc/lev.py
+++ datagrok/misc/lev.py
@@ -3,7 +3,6 @@ from datagrok.misc import memoized
 @memoized
 def lev(s1, s2):
     """Returns the levenshtein distance between strings s1 and s2."""
-    #print s1, s2
     if not len(s1): return len(s2)
     if not len(s2): return len(s1)
     return min(
@@ -12,9 +11,12 @@ def lev(s1, s2):
             lev(s1[:-1], s2[:-1]) + (0 if s1[-1] == s2[-1] else 1)
             )
 
-print lev.__doc__
-print lev('kitten', 'sitting')
-print lev('kitten', 'kitten')
-print lev('', '')
-print lev('confide', 'deceit')
-print lev('CUNsperrICY', 'conspiracy')
+if __name__ == "__main__":
+    for s1, s2 in [
+        ('kitten', 'sitting'),
+        ('kitten', 'kitten'),
+        ('', ''),
+        ('confide', 'deceit'),
+        ('CUNsperrICY', 'conspiracy'),
+        ]
+    print(lev(s1, s2))
